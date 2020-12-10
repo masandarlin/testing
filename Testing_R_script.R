5105b16e13a6b25fd8b79c4b7bea3004f8703b0b
ES-IT <masandarlin@gmail.com>
2020-12-10 14:47:38 +0630

    2nd commit & Push

--- Testing_R_script.R
+++ Testing_R_script.R
@@ -1,2 +1,12 @@
 print("This file was created within RStudio")
 print("And now it lives on GitHub")
+
+q()
+license()
+citation()
+1+1
+matrix(c(1,2,3,4,5,6,7,8),nrow=4,ncol=2)
+example <- matrix(c(1, 2, 3, 4, 5, 6, 7, 8), nrow = 4, ncol = 2)
+View(example)
+setwd("C:/Users/ES-IT/Desktop/Learning")
+plot(100,20)
